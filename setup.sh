846013a7fe3372afb8842cb891910d57060a60c7
root <root@devlab-centos.(none)>
2016-01-19 23:44:58 -0500

    added formatted output, redirects stdout and err to log

--- setup.sh
+++ setup.sh
@@ -2,21 +2,80 @@
 
 # Setup script for a droplet that installs various missing packages
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+CLR='\033[0m'
+
+LOG="/tmp/setup.log"
+
+function greenprint {
+	printf "[$GREEN%s$CLR]\n" $1
+}
+
+function redprint {
+	printf "[$RED%s$CLR]\n" $1
+}
+
+function pipinstall {
+	printf "Pip-installing %s... " "$1"
+	pip install $1 2>&1 >> $LOG 2>&1
+	if [ "$?" != "0" ]; then
+		redprint "Failed"
+		return 1
+	else
+		greenprint "Success"
+		return 0
+	fi
+}
+
+function yuminstall {
+	printf "Yum-installing %s... " "$1"
+	yum -y install $1 >> $LOG 2>&1
+	if [ "$?" != "0" ]; then
+		redprint "Failed"
+		return 1
+	else
+		greenprint "Success"
+		return 0
+	fi
+}
+
+function run_cmd {
+	printf "Performing %s... " "$1"
+	$1 >> $LOG 2>&1
+	if [ "$?" != "0" ]; then
+		redprint "Failed"
+		return 1
+	else
+		greenprint "Success"
+		return 0
+	fi
+}
+
+
+# Clear log
+rm -f $LOG
+
 cd /tmp
 
-yum -y clean all
-yum -y update
+# Clean yum cache and update list
+run_cmd "yum -y clean all"
+run_cmd "yum -y update"
 
-yum -y install man
-yum -y install man-pages
-yum -y install vim
-yum -y install git
-yum -y install wget
+# Install some pkgs through yum
+yuminstall man
+yuminstall man-pages
+yuminstall vim
+yuminstall git
+yuminstall wget
 
-wget https://bootstrap.pypa.io/get-pip.py
-python get-pip.py
+# Install pip
+run_cmd "wget https://bootstrap.pypa.io/get-pip.py"
+run_cmd "python get-pip.py"
 
-pip install virtualenv
-pip install virtualenvwrapper
+# Install some python modules with pip
+pipinstall virtualenv
+pipinstall virtualenvwrapper
 
 
+printf "\n\nView detailed output in $LOG\n"
