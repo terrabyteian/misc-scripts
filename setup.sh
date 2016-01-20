420ffba3ff1145d74d8413b692f514b51cf63e1c
root <root@devlab-centos.(none)>
2016-01-19 21:52:30 -0500

    added pip and python virtenv

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 # Setup script for a droplet that installs various missing packages
 
+cd /tmp
+
 yum -y clean all
 yum -y update
 
@@ -9,3 +11,12 @@ yum -y install man
 yum -y install man-pages
 yum -y install vim
 yum -y install git
+yum -y install wget
+
+wget https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+
+pip install virtualenv
+pip install virtualenvwrapper
+
+
